047a843911a30f8ced2a77e8ab30c00adae12e7f
yusufakg <101368497+yusufakg@users.noreply.github.com>
2024-06-30 20:26:24 +0200

    feat(techradar): account for blipRadius for radar angles

--- storybook/stories/techradar/Techradar.stories.tsx
+++ storybook/stories/techradar/Techradar.stories.tsx
@@ -11,36 +11,36 @@ const meta: Meta<typeof Radar> = {
 export default meta
 type Story = StoryObj<typeof Radar>
 
-const sectors = ['sector1', 'sector2', 'sector3', 'sector4', 'sector4', 'sector4', 'sector4']
+const sectors = ['sector1', 'sector2', 'sector3', 'sector4']
 
-const rings = ['ring1', 'ring2', 'ring3', 'ring3', 'ring3']
+const rings = ['ring1', 'ring2', 'ring3']
 
 const blips = [
     { sector: 's0', ring: 'r0', id: 'blip1' },
-    // { sector: 's0', ring: 'r0', id: 'blip2' },
-    // { sector: 's0', ring: 'r0', id: 'blip3' },
-    // { sector: 's0', ring: 'r0', id: 'blip4' },
-    // { sector: 's0', ring: 'r0', id: 'blip5' },
-    // { sector: 's0', ring: 'r0', id: 'blip6' },
-    // { sector: 's0', ring: 'r0', id: 'blip7' },
-    // { sector: 's0', ring: 'r0', id: 'blip8' },
-    { sector: 's3', ring: 'r2', id: 'blip9' },
-    { sector: 's3', ring: 'r2', id: 'blip10' },
-    { sector: 's3', ring: 'r2', id: 'blip11' },
-    { sector: 's3', ring: 'r2', id: 'blip12' },
-    { sector: 's3', ring: 'r2', id: 'blip13' },
-    { sector: 's3', ring: 'r2', id: 'blip14' },
-    { sector: 's3', ring: 'r2', id: 'blip15' },
-    { sector: 's3', ring: 'r2', id: 'blip16' },
-    { sector: 's3', ring: 'r2', id: 'blip17' },
-    { sector: 's3', ring: 'r2', id: 'blip18' },
-    { sector: 's3', ring: 'r2', id: 'blip19' },
-    { sector: 's3', ring: 'r2', id: 'blip20' },
+    { sector: 's0', ring: 'r0', id: 'blip2' },
+    { sector: 's0', ring: 'r0', id: 'blip3' },
+    { sector: 's0', ring: 'r0', id: 'blip4' },
+    { sector: 's1', ring: 'r1', id: 'blip5' },
+    { sector: 's1', ring: 'r1', id: 'blip6' },
+    { sector: 's1', ring: 'r1', id: 'blip7' },
+    { sector: 's1', ring: 'r1', id: 'blip8' },
+    { sector: 's2', ring: 'r2', id: 'blip9' },
+    { sector: 's2', ring: 'r2', id: 'blip10' },
+    { sector: 's2', ring: 'r2', id: 'blip11' },
+    { sector: 's2', ring: 'r2', id: 'blip12' },
+    { sector: 's2', ring: 'r2', id: 'blip13' },
+    { sector: 's2', ring: 'r2', id: 'blip14' },
+    { sector: 's2', ring: 'r2', id: 'blip15' },
+    { sector: 's2', ring: 'r2', id: 'blip16' },
+    { sector: 's2', ring: 'r2', id: 'blip17' },
+    { sector: 's2', ring: 'r2', id: 'blip18' },
+    { sector: 's2', ring: 'r2', id: 'blip19' },
+    { sector: 's2', ring: 'r2', id: 'blip20' },
 ]
 
 const commonProperties = {
-    width: 500,
-    height: 500,
+    width: 700,
+    height: 700,
     margin: { top: 60, right: 80, bottom: 30, left: 80 },
     sectorData: sectors,
     ringData: rings,
